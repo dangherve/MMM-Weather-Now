68fca1abad7b4919e01b171c6b15141a765b80a7
dangherve <dangherve@gmail.com>
2026-03-07 01:25:45 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/nigel-daniels/MMM-Weather-Now.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/MMM-Weather-Now.git
+git push --force --set-upstream origin master
